6091fba0e49831f79cf927df84800c8dba80c240
CalGordonHallCounty <cgordon@hallcounty.org>
2022-09-26 18:09:54 -0400

    Imported models - Added admin support (/admin to URL to see)

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Profile
+from django.contrib import admin
+
+
+# Register your models here.
+# admin.site.register(Profile)
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+
+    fieldsets = (
+        ('User Information:', {
+          'fields': ('user', 'birthday', 'address', 'city', 'state', 'zip', 'phone')
+        }),
+        ('Friends List:', {
+            'fields': ('friends',)
+        })
+
+    )
+
+    filter_horizontal = ['friends']
